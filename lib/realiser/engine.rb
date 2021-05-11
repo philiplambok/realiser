348ea9dbfeb5249cb0355a38c8488c24858244d5
Philip Lambok <philiplambok71@gmail.com>
2021-05-11 16:31:18 +0700

    Add assets precompile.

--- lib/realiser/engine.rb
+++ lib/realiser/engine.rb
@@ -1,5 +1,8 @@
 module Realiser
   class Engine < ::Rails::Engine
     isolate_namespace Realiser
+    initializer 'realiser.assets.precompile' do |app|
+      app.config.assets.precompile += %w[application.css]
+    end
   end
 end
